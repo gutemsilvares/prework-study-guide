7f1b34a1340844f7d853d743d46f91db8ae6bc77
Agutembergues <gutemsilvares@hotmail.com>
2023-01-21 00:03:46 +0000

    feature/add-js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
-var shape = 'circle';
-var topics = 'HTML, CSS, Git, Javascript';
-console.log(topics);
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+}
